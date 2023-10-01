30e6dabb6b268ea91cb8a551f10a0f82172908a2
krishna9809 <multiatoms980996@gmail.com>
2023-10-01 23:04:27 +0530

    Javascript-basics-2

---
+++ Day036/Javascript-basics-2/script.js
@@ -0,0 +1,38 @@
+a = 5;
+b = 10;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a%b);
+console.log(a/b)
+console.log(a**b)
+
+
+console.log((++a)*(--b))
+console.log((a++)*(--b))
+
+
+
+console.log(a>b)
+console.log(a<b)
+console.log(a===b)
+console.log(a!==b)
+
+let c = 1
+let d = '1'
+
+console.log(c==d)
+console.log(c===d)
+
+let age = 17
+
+let status = (age>=18)? 'Yes vote' : 'No voteadasdsadsdsadaddads';
+
+console.log(status);
+
+
+
+
+
+
+
