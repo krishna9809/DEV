2bbaa7da4a0e19fea615733f419a82dc72271ada
krishna9809 <multiatoms980996@gmail.com>
2023-10-07 22:37:31 +0530

    javascript

---
+++ Day042/Javascript/script.js
@@ -0,0 +1,47 @@
+// Math Object
+
+console.log(Math.random());
+// console.log(Math.PI());
+console.log(Math.round(4.5));
+console.log(Math.min(4,6,2,1,4));
+
+
+// String Object
+
+let name = new String('krishna');
+
+console.log(name[0]);
+
+let message = "This is my message";
+
+let words = message.split(' ');
+
+console.log(words);
+message.toUpperCase
+
+
+// Template Litral
+
+let text = `hello world`
+
+let text1 =`heloo "sfsf"Sfsfs`;
+
+let text2 = `heloo
+fssff
+fsfsfsf
+sfsfffffffff`
+
+console.log(text);
+console.log(text1);
+console.log(text2);
+
+console.log(`hello ${text}`);
+
+// Date and Time
+
+console.log(new Date('June 20 1998'));
+
+console.log(text);
+let dtae3 = new Date()
+
+dtae3.setHours
