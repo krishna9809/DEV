0b506a3d8a616d6560f405a031bdbcf34a255499
krishna9809 <multiatoms980996@gmail.com>
2023-10-16 22:11:09 +0530

    DOM-3

---
+++ Day051/Javascript-DOM/script.js
@@ -0,0 +1,9 @@
+// Remove Element
+
+const childElement = document.getElementById("childElementId");
+const parentElement = childElement.parentElement;
+
+parentElement.removeChild(childElement);
+
+const el = document.getElementById("some-element");
+el.setAttribute("style", "background-color:darkblue;");
