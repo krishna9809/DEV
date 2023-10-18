47d634c05446392c6dd1289ef4008e74f4d5cf10
krishna9809 <multiatoms980996@gmail.com>
2023-10-18 22:04:30 +0530

    Event-Listener

---
+++ Day053/script.js
@@ -0,0 +1,8 @@
+function eventFunction() {
+    console.log('I have Clicked on the Documenet');
+}
+
+document.addEventListener('click', eventFunction);
+// document.removeEventListener('click', eventFunction);
+
+
