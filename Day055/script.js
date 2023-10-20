79c6a35ebfd908b843acadacb8cbf9ed314caceb
krishna9809 <multiatoms980996@gmail.com>
2023-10-20 23:07:15 +0530

    Day 55

---
+++ Day055/script.js
@@ -0,0 +1,14 @@
+let con = document.querySelector('.head');
+
+con.addEventListener('click', function() {
+    con.style.backgroundColor = 'red';
+});
+
+con.removeEventListener('click', function() {
+    con.style.backgroundColor = 'red';
+});
+
+
+
+
+
