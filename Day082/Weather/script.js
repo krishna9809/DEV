3fef0054470c692789d80606eec99da0aaedfd84
krishna9809 <multiatoms980996@gmail.com>
2023-11-20 22:23:05 +0530

    Weather App UI

---
+++ Day082/Weather/script.js
@@ -0,0 +1,18 @@
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
+
+const userContainer = document.querySelector(".wrapper");
+
+const grantAccessContainer = document.querySelector(".grant-location-container");
+const searchForm = document.querySelector("[data-searchForm]");
+
+const laodingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".your-2");
+
+
+//
+
+let currentTab = userTab;
+
+// const API_KEY 
+
